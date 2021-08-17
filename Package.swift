2c2888113dd85494e94b9ef1e22fc38ffdede288
Mohamed Salah Nassar <mohamedsalah@nagarrodxb.com>
2021-08-17 16:37:39 +0400

    Removed specifying the type of the product to .dynamic in package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,6 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "FTDiffieHellman",
-            type: .dynamic,
             targets: ["FTDiffieHellman"]),
     ],
     dependencies: [
